0abe4c000915b317304f4e7e3e6f7624884fe9cc
James Logue <logueja@gmail.com>
2017-10-21 17:35:40 -0600

    Add idea rating event listeners and text updater

--- scripts/script.js
+++ scripts/script.js
@@ -3,26 +3,45 @@ var $bodyInput = $('#body-input');
 var $saveButton = $('.button-save');
 
 var $searchInput = $('#search-input');
-
 var $cardsContainer = $('#cards-container')
 
-var ideaCard = $('.idea-card')
+// don't actually need these yet.. may later though
+var $ideaCard = $('.idea-card')
 var $cardDeleteButtons = $('.card-delete-button');
 var $cardHeader = $('.card-header');
 var $cardContent = $('.card-content');
-var $ideaQuality = $('.idea-quality');
+
+//
+
 
 var $upvoteButton = $('.upvote-button');
 var $downvoteButton = $('.downvote-button');
+var $ideaQuality = $('.idea-quality');
 
-var cardKey;
+// var cardKey;
 
 $(document).ready( function() {
   displayStorage();
-
 })
 
 $saveButton.on('click', makeCard);
+
+$searchInput.keyup(searchCards)
+
+$cardsContainer.on('click', '.idea-card .card-delete-button', function() {
+  var $deletedCard = $(this)
+  deleteCard(this)
+});
+
+$cardsContainer.on('click', '.idea-card .upvote-button', function() {
+  var $thisVote = $(this);
+  setIdeaQuality($thisVote, 'upvote')
+});
+
+$cardsContainer.on('click', '.idea-card .downvote-button', function() {
+  var $thisVote = $(this);
+  setIdeaQuality($thisVote, 'downvote')
+});
 
 function makeCard(e) {
   e.preventDefault();
@@ -30,9 +49,11 @@ function makeCard(e) {
   var cardKey = Date.now();
   var titleVal = $titleInput.val();
   var bodyVal = $bodyInput.val();
-  var ideaQuality = 'swill';
+  var ideaQuality = 'plausible';
   prependCards(cardKey, titleVal, bodyVal, ideaQuality);
   addToStorage(cardKey, titleVal, bodyVal, ideaQuality);
+
+  resetInputs();
 }
 
 function prependCards(key, title, body, quality) {
@@ -70,22 +91,46 @@ function displayStorage () {
     var thisCardKey = JSON.parse(Object.values(localStorage)[i]).cardKey;
     var thisCardTitle = JSON.parse(Object.values(localStorage)[i]).title;
     var thisCardBody = JSON.parse(Object.values(localStorage)[i]).body;
-    var thisIdeaQuality = JSON.parse(Object.values(localStorage)[i]).body;
+    var thisIdeaQuality = JSON.parse(Object.values(localStorage)[i]).quality;
 
     prependCards(thisCardKey, thisCardTitle, thisCardBody, thisIdeaQuality)
   }
 }
 
-$cardsContainer.on('click', '.idea-card .card-delete-button', function() {
-  var $deletedCard = $(this)
-  deleteCard(this)
-});
-
 function deleteCard (card) {
-  $thisKey = $(card).closest('.idea-card').attr('id')
-  card.closest('.idea-card').remove()
-  localStorage.removeItem($thisKey)
+  thisKey = $(card).closest('.idea-card').attr('id');
+  card.closest('.idea-card').remove();
+  localStorage.removeItem(thisKey);
 }
 
+function resetInputs () {
+  $titleInput.val("");
+  $bodyInput.val("");
+  $searchInput.val("");
+}
 
-//this.prop(id.val)
+function searchCards(e) {
+  e.preventDefault();
+
+  console.log($searchInput.val())
+}
+
+function setIdeaQuality (card, vote) {
+  var thisKey = $(card).closest('.idea-card').attr('id');
+  var $thisIdea = JSON.parse(localStorage.getItem(thisKey))
+
+  if ($thisIdea.quality === 'plausible' && vote === 'upvote') {
+    $(`#${thisKey} .idea-quality`).text('genius')
+    /// NEEDS TO UPDATE IN JSON  HERE
+  } else if ($thisIdea.quality === 'plausible' && vote === 'downvote') {
+    $(`#${thisKey} .idea-quality`).text('swill')
+    /// NEEDS TO UPDATE IN JSON  HERE
+  } else if ($thisIdea.quality === 'swill' && vote === 'upvote') {
+    $(`#${thisKey} .idea-quality`).text('plausible')
+    /// NEEDS TO UPDATE IN JSON  HERE
+  } else if ($thisIdea.quality === 'genius' && vote === 'downvote') {
+    $(`#${thisKey} .idea-quality`).text('plausible')
+    // UPDATE JSON HERE
+  };
+
+}
